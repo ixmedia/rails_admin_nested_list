ff5b9531253e66327a303639a52d719a0c5446a9
Sebastien Asselin <sebastien@ixmedia.com>
2023-01-25 11:30:50 -0500

    Fix loading of CSS

--- lib/rails_admin_nested_list/engine.rb
+++ lib/rails_admin_nested_list/engine.rb
@@ -2,9 +2,9 @@ require 'rails'
 require 'rails_admin_nested_list'
 
 module RailsAdminNestedList
-  class Engine < Rails::Engine
+  class Engine < ::Rails::Engine
     initializer "RailsAdminNestedList precompile hook", group: :all do |app|
-      app.config.assets.precompile += %w(rails_admin/rails_admin_nestable_list.css)
+      app.config.assets.precompile += %w(rails_admin/rails_admin_nested_list.css)
     end
   end
 end
